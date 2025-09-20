5582e59ec88e4b3e73ddec7a9ed1d29342938852
Deylin <deylinbo91@gmail.com>
2025-09-20 00:30:14 -0600

    Actualizar grupo-unbanchat.js

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,15 +1,15 @@
 let handler = async (m, { conn }) => {
-if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '〽️l🔥 *¡Este chat no está registrado!*', m, fake)
+if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '*ԋσʅα, ɳσ ҽɳƈσɳƚɾσ ҽʂƚҽ ƈԋαƚ ҽɳ ɱι Ⴆαʂҽ ԃҽ ԃαƚσʂ.*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '👑 *¡ᴀsᴛʀᴏ-ʙᴏᴛ ɴᴏ ᴇsᴛᴀ ʙᴀɴᴇᴀᴅᴏ ᴇɴ ᴇsᴛᴇ ᴄʜᴀᴛ!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, `*¡𝚂𝚒𝚐𝚘 𝚊𝚌𝚝𝚒𝚟𝚊 𝚎𝚗 𝚎𝚜𝚝𝚎 𝚌𝚑𝚊𝚝!*`, m, fake)
 chat.isBanned = false
-await conn.reply(m.chat, '⚡ *¡ᴀsᴛʀᴏ-ʙᴏᴛ ʏᴀ ғᴜᴇ ᴅᴇsʙᴀɴᴇᴀᴅᴏ ᴇɴ ᴇsᴛᴇ ᴄʜᴀᴛ!*', m, fake)
+await conn.reply(m.chat, '🪷 *¡𝚄𝚏𝚏, 𝚢𝚊 𝚖𝚎 𝚎𝚜𝚝𝚊𝚋𝚊 𝚊𝚋𝚞𝚛𝚛𝚒𝚎𝚗𝚍𝚘... 𝙷𝚘𝚕𝚊 𝚍𝚎 𝚗𝚞𝚎𝚟𝚘!*', m, fake)
 }
-handler.help = ['unbanchat'];
-handler.tags = ['grupo'];
+handler.help = ['unbanchat']
+handler.tags = ['grupo']
 handler.command = ['unbanchat','desbanearchat','desbanchat']
 handler.admin = true 
-handler.botadmin = true
+handler.botAdmin = true
 handler.group = true
 
 export default handler
