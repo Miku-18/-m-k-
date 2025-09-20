d328d495969a1f7cbe000434c0131a2402dec644
Deylin <deylinbo91@gmail.com>
2025-09-20 00:43:59 -0600

    Actualizar owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -1,15 +1,24 @@
 let handler = async (m, { conn, text, command }) => {
-let id = text ? text : m.chat  
-let chat = global.db.data.chats[m.chat]
-chat.welcome = false
-await conn.reply(id, `*Me despido de esta galaxia* 🚀`) 
-await conn.groupLeave(id)
-try {  
-chat.welcome = true
-} catch (e) {
-await m.reply(`${fg}`) 
-return console.log(e)
-}}
+    let id = text ? text : m.chat  
+    let chat = global.db.data.chats[m.chat]
+    chat.welcome = false
+
+    await conn.reply(id, `🎀💙🌸✨🎀💙🌸✨🎀
+*¡Me despido de esta galaxia~!* 🌙💫
+Que las notas musicales 🎶 y los sueños ✨ te acompañen 💖
+Nos vemos en otra dimensión 🌸💙
+🎀💙🌸✨🎀💙🌸✨🎀`) 
+
+    await conn.groupLeave(id)
+
+    try {  
+        chat.welcome = true
+    } catch (e) {
+        await m.reply(`❌ Oops, ocurrió un error inesperado 💦`) 
+        return console.log(e)
+    }
+}
+
 handler.command = ['salir','leavegc','salirdelgrupo','leave']
 handler.group = true
 handler.owner = true
